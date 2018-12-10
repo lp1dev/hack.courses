949efd2701ba5ca5cf71a0c6353863901593f489
lp1 <mail@lp1.eu>
2018-12-10 15:13:25 +0100

    updated td02

--- Programming/Node_Angular/TDs_node/td02.js
+++ Programming/Node_Angular/TDs_node/td02.js
@@ -1,7 +1,7 @@
 /* TD02 - We're up to something, aren't we ?
 
-Le but de cet exercice est de compter 
-le nombre de lignes d'un fichier disponible 
+Le but de cet exercice est de compter de manière
+asynchrone le nombre de lignes d'un fichier disponible 
 en ligne (correspondant à URL).
 
 /* TD Part */
@@ -10,10 +10,12 @@ const axios = require('axios')
 const URL = 'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/probable-v2-top207.txt'
 
 function countLines() {
-    const lines = 0
-    return lines
 }
 
 /* Testing Part */
 
-console.log(countLines() == 207 ? 'TD02: OK' : 'TD02: KO')
+countLines()
+    .then(lines => {
+        console.log(lines == 208 ? 'TD02: OK' : 'TD02: KO')
+    })
+    .catch(e => console.error(e))
