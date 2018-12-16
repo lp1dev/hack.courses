93fed6f9ab8da531f1ebc0e97c2c6267d60e37cf
Jeremie A <mail@lp1.eu>
2018-12-16 20:51:04 +0100

    Finished level0

---
+++ Programming/JS/TDs/level0/td07.js
@@ -0,0 +1,22 @@
+/* td07 - Les tableaux
+
+Cet exercice est assez similaire au td06,
+cette-fois ci vous devez implémenter la 
+fonction td07 qui prend en paramètre un
+nombre nommé max et retourne un tableau 
+contenant les nombres de 0 à max (inclus).
+
+*/
+
+/* TD Part */
+
+function td07(max) {
+}
+
+/* Testing Part */
+const array = '0'.repeat(999).split('').map((i, id) => id);
+if (td07(999) === array) {
+    console.log('TD07:: OK');
+} else {
+    console.log('TD07:: KO');
+}
